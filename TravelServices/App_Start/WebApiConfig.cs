845b0a7b0f4556825abe95d488d31a1a0fa65b1e
Ly111999 <39553327+Ly111999@users.noreply.github.com>
2018-12-29 10:34:29 +0700

    asm webservices

--- TravelServices/App_Start/WebApiConfig.cs
+++ TravelServices/App_Start/WebApiConfig.cs
@@ -14,6 +14,14 @@ namespace TravelServices
             // Web API routes
             config.MapHttpAttributeRoutes();
 
+            var json = GlobalConfiguration.Configuration.Formatters.JsonFormatter;
+            json.SerializerSettings.PreserveReferencesHandling = Newtonsoft.Json.PreserveReferencesHandling.None;
+            json.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Serialize;
+            config.Formatters.Remove(config.Formatters.XmlFormatter);
+
+            config.Formatters.JsonFormatter.SupportedMediaTypes.Add(new System.Net.Http.Headers.MediaTypeHeaderValue("multipart/form-data"));
+
+
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
                 routeTemplate: "api/{controller}/{id}",
